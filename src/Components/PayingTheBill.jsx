e1b5fff0008a45c8393383b573e5bd6f7f4e75e6
mohammad <m.tehrani@gmail.com>
2019-04-17 15:35:43 +0430

    push

--- src/Components/PayingTheBill.jsx
+++ src/Components/PayingTheBill.jsx
@@ -33,6 +33,176 @@ class PayingTheBill extends Component {
             });
     }
 
+    checkBill(y)
+    {
+        let a = [];
+        let b = [];
+        let c = 0;
+        let d = 0;
+        for(let i = 0;i < y.length;i++)
+        {
+            a[i] = parseInt((y / Math.pow(10,i)) % 10);
+        }
+        b[1] = a[1]*2;
+        b[2] = a[2]*3;
+        b[3] = a[3]*4;
+        b[4] = a[4]*5;
+        b[5] = a[5]*6;
+        c = b[1] + b[2] + b[3] + b[4] + b[5];
+        if(a[6])
+        {
+            b[6] = a[6]*7;
+            c = b[1] + b[2] + b[3] + b[4] + b[5] + b[6];
+        }
+        if(a[7])
+        {
+            b[6] = a[6]*7;
+            b[7] = a[7]*2;
+            c = b[1] + b[2] + b[3] + b[4] + b[5] + b[6] + b[7];
+        }
+        if(a[8])
+        {
+            b[6] = a[6]*7;
+            b[7] = a[7]*2;
+            b[8] = a[8]*3;
+            c = b[1] + b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8];
+        }
+        if(a[9])
+        {
+            b[6] = a[6]*7;
+            b[7] = a[7]*2;
+            b[8] = a[8]*3;
+            b[9] = a[9]*4;
+            c = b[1] + b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8] + b[9];
+        }
+        if(a[10])
+        {
+            b[6] = a[6]*7;
+            b[7] = a[7]*2;
+            b[8] = a[8]*3;
+            b[9] = a[9]*4;
+            b[10] = a[10]*5;
+            c = b[1] + b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8] + b[9] + b[10];
+        }
+        if(a[11])
+        {
+            b[6] = a[6]*7;
+            b[7] = a[7]*2;
+            b[8] = a[8]*3;
+            b[9] = a[9]*4;
+            b[10] = a[10]*5;
+            b[11] = a[11]*6;
+            c = b[1] + b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8] + b[9] + b[10] + b[11];
+        }
+        if(a[12])
+        {
+            b[6] = a[6]*7;
+            b[7] = a[7]*2;
+            b[8] = a[8]*3;
+            b[9] = a[9]*4;
+            b[10] = a[10]*5;
+            b[11] = a[11]*6;
+            b[12] = a[12]*7;
+            c = b[1] + b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8] + b[9] + b[10] + b[11] + b[12];
+        }
+        d = 11 - (c%11);
+        this.setState({
+            bill: a[1],
+            billValid: d,
+            valid: a[0]
+        });
+        if (d == a[0])
+        {
+            return true;
+        }
+        return false;
+    }
+
+    checkPayment(x)
+    {
+        let a = [];
+        let b = [];
+        let c = 0;
+        let d = 0;
+        let e = parseInt(x/100000) * 1000;
+        for(let i = 0;i < x.length;i++)
+        {
+            a[i] = parseInt((x / Math.pow(10,i)) % 10);
+        }
+        b[2] = a[2]*2;
+        b[3] = a[3]*3;
+        b[4] = a[4]*4;
+        b[5] = a[5]*5;
+        c = b[2] + b[3] + b[4] + b[5];
+        if(a[6])
+        {
+            b[6] = a[6]*6;
+            c = b[2] + b[3] + b[4] + b[5] + b[6];
+        }
+        if(a[7])
+        {
+            b[6] = a[6]*6;
+            b[7] = a[7]*7;
+            c = b[2] + b[3] + b[4] + b[5] + b[6] + b[7];
+        }
+        if(a[8])
+        {
+            b[6] = a[6]*6;
+            b[7] = a[7]*7;
+            b[8] = a[8]*2;
+            c = b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8];
+        }
+        if(a[9])
+        {
+            b[6] = a[6]*6;
+            b[7] = a[7]*7;
+            b[8] = a[8]*2;
+            b[9] = a[9]*3;
+            c = b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8] + b[9];
+        }
+        if(a[10])
+        {
+            b[6] = a[6]*6;
+            b[7] = a[7]*7;
+            b[8] = a[8]*2;
+            b[9] = a[9]*3;
+            b[10] = a[10]*4;
+            c = b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8] + b[9] + b[10];
+        }
+        if(a[11])
+        {
+            b[6] = a[6]*6;
+            b[7] = a[7]*7;
+            b[8] = a[8]*2;
+            b[9] = a[9]*3;
+            b[10] = a[10]*4;
+            b[11] = a[11]*5;
+            c = b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8] + b[9] + b[10] + b[11];
+        }
+        if(a[12])
+        {
+            b[6] = a[6]*6;
+            b[7] = a[7]*7;
+            b[8] = a[8]*2;
+            b[9] = a[9]*3;
+            b[10] = a[10]*4;
+            b[11] = a[11]*5;
+            b[12] = a[12]*6;
+            c = b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8] + b[9] + b[10] + b[11] + b[12];
+        }
+        d = 11 - (c%11);
+        this.setState({
+            payment: e,
+            paymentValid: d,
+            payValid: a[1]
+        });
+        if (d == a[1])
+        {
+            return true;
+        }
+        return false;
+    }
+
     componentWillMount() {
         if (!(this.Auth.getToken())) {
             window.location.replace('/Components/Login');
@@ -57,33 +227,35 @@ class PayingTheBill extends Component {
 
     handleFormSubmit(e){
         e.preventDefault();
-        this.setState({
-            load1: !this.state.load1
-        })
-        const url = this.Auth.getDomain() + '/user/bank/bill-payment';
-        const formData = {
-            billing_code: this.state.billing_code,
-            payment_code: this.state.payment_code,
-            public_key: this.state.public_key,
-        };
-        const headers = {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.Auth.getToken()}`,
-        };
-        var config = { headers };
-        return axios.post(url, formData, config)
-            .then(response => {
-                this.setState({
-                    xdr: response.data.xdr,
-                    bill_payment_id: response.data.bill_payment_id
-                });
-            })
-            .catch(err =>{
-                this.setState({
-                    load1: false
+        if(this.checkBill(this.state.billing_code) && this.checkPayment(this.state.payment_code)) {
+            this.setState({
+                load1: !this.state.load1
+            });
+            const url = this.Auth.getDomain() + '/user/bank/bill-payment';
+            const formData = {
+                billing_code: this.state.billing_code,
+                payment_code: this.state.payment_code,
+                public_key: this.state.public_key,
+            };
+            const headers = {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${this.Auth.getToken()}`,
+            };
+            var config = {headers};
+            return axios.post(url, formData, config)
+                .then(response => {
+                    this.setState({
+                        xdr: response.data.xdr,
+                        bill_payment_id: response.data.bill_payment_id
+                    });
                 })
-            })
+                .catch(err => {
+                    this.setState({
+                        load1: false
+                    })
+                })
+        }
     }
 
     handleForSignWithSecretKey(e){
@@ -124,6 +296,33 @@ class PayingTheBill extends Component {
     }
 
     render() {
+        let error='';
+        if(!(this.state.billValid == this.state.valid && this.state.paymentValid == this.state.payValid))
+        {
+            error = <div className="col-12"><div className="col-12 bg-danger text-light p-2 mb-2 rounded shadow-lg text-center mb-5">This billing code or payment code is incorrect</div></div>
+        }
+        const project = () => {
+            switch (this.state.bill) {
+                case 1:
+                    return <h4 className="col-12 text-center">Bill Type : Water Bill</h4>;
+                case 2:
+                    return <h4 className="col-12 text-center">Bill Type : Electric Bill</h4>;
+                case 3:
+                    return <h4 className="col-12 text-center"> Bill Type : gas Bill</h4>;
+                case 4:
+                    return <h4 className="col-12 text-center">Bill Type : Landline Bill</h4>;
+                case 5:
+                    return <h4 className="col-12 text-center">Bill Type : Phone Bill</h4>;
+                case 6:
+                    return <h4 className="col-12 text-center">Bill Type : Municipal Tax Complaint Bill</h4>;
+                case 7:
+                    return <h4 className="col-12 text-center">Bill Type : Bill</h4>;
+                case 8:
+                    return <h4 className="col-12 text-center">Bill Type : Tax Bill</h4>;
+                case 9:
+                    return <h4 className="col-12 text-center">Bill Type : Road traffic offenses Bill</h4>;
+            }
+        }
         let loader = "";
         let loader2 ="";
         if(this.state.load1 == false)
@@ -160,6 +359,7 @@ class PayingTheBill extends Component {
             return (
                 <div className="col-sm-8 col-12 clearfix mx-auto">
                     <div className="row">
+                        {error}
                         <h2 className="col-12 text-light text-center font-weight-bold mb-5">Paying the bill</h2>
                         <form className="col-12" onSubmit={this.handleFormSubmit}>
                             <label className="col-12">
@@ -183,12 +383,16 @@ class PayingTheBill extends Component {
             return (
                 <div className="col-sm-8 col-12 clearfix mx-auto">
                     <div className="row">
-                        <h2 className="col-12 text-light text-center font-weight-bold mb-5">Paying the bill</h2>
+                        <h2 className="col-12 text-light font-weight-bold mb-5 text-center">Paying the bill</h2>
+                        <div className="col-12 text-cenetr text-light font-weight-bold">
+                            {project()}
+                            <h4 className="col-12 text-center mt-3">Bill Amount : {this.state.payment} IRR</h4>
+                        </div>
                         <form className="col-12" onSubmit={this.handleForSignWithSecretKey}>
                             <label className="col-12">
                                 <div className="row shadow-lg">
-                                    <span className="col-3 text-center text-light p-2 rounded-left bg-warning">Secret key</span>
-                                    <input className="col-9 text-center rounded-right p-2" placeholder="SB3JKIKJ7ECA2GBB55KG55KRHUILGDHXZ5GZ5WBWYOFS7KU6JT73C7HX" name="secret_key" type="text" onChange={this.handleChange}/>
+                                    <span className="col-3 text-center text-light p-2 rounded-left bg-warning mt-3">Secret key</span>
+                                    <input className="col-9 text-center rounded-right p-2 mt-3" placeholder="SB3JKIKJ7ECA2GBB55KG55KRHUILGDHXZ5GZ5WBWYOFS7KU6JT73C7HX" name="secret_key" type="text" onChange={this.handleChange}/>
                                 </div>
                             </label>
                             {loader2}
