9f58308e719ae1210f426691fd58f0a12d20857a
mohammad <m.tehrani@gmail.com>
2019-04-29 12:17:42 +0430

    push

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -14,7 +14,52 @@ class Dashboard extends Component {
         super();
         this.Auth = new AuthService();
         this.assetAmount = this.assetAmount.bind(this);
-        this.state = {}
+        this.handleChange1 = this.handleChange1.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+        this.handleConvertRate = this.handleConvertRate.bind(this);
+        this.state = {
+            formSelect1: 'XLM',
+            formSelect2: 'XLM',
+        }
+    }
+
+    handleChange1(e)
+    {
+        this.setState({
+            formSelect1: e.target.value,
+        });
+        console.log(this.state.formSelect1);
+    }
+
+    handleChange2(e)
+    {
+        this.setState({
+            formSelect2: e.target.value,
+        });
+        console.log(this.state.formSelect2);
+    }
+
+    handleConvertRate(e)
+    {
+        this.setState({
+            [e.target.name]:e.target.value,
+        });
+        if(e.target.name == "amount") {
+            let amount = e.target.value;
+            var url = `${this.Auth.domain}/user/convert?type=` + this.state.formSelect1 + `TO` + this.state.formSelect2 + `&amount=` + amount;
+            const headers = {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${this.Auth.getToken()}`,
+            };
+            var config = {headers};
+            return axios.get(url, config)
+                .then(response => {
+                    this.setState({
+                        price: parseFloat(response.data.result).toFixed(2)
+                    });
+                });
+        }
     }
 
     componentWillMount() {
@@ -62,6 +107,37 @@ class Dashboard extends Component {
         return (
             <div className="col-sm-8 col-12 clearfix mx-auto">
                 <div className="row">
+                    <div className="col-12">
+                        <div className="col-12 mx-auto">
+                            <div className="row">
+                                <div className="col-sm-5 col-12 mt-3 mb-3">
+                                    <div className="col-12">
+                                        <div className="row">
+                                            <input className="col-6 pt-2 pb-2 rounded-left text-center" name="amount" type="text" onChange={this.handleConvertRate}/>
+                                            <select className="col-6 pt-2 pb-2 rounded-right bg-warning text-center border border-warning text-light" name="type" onChange={this.handleChange1}>
+                                                <option type="0">XLM</option>
+                                                <option type="1">XIR</option>
+                                                <option type="2">IRR</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-sm-2 col-12 mt-3 mb-3 text-center text-light pt-2">Convert rate</div>
+                                <div className="col-sm-5 col-12 mt-3 mb-3">
+                                    <div className="col-12">
+                                        <div className="row">
+                                            <div className="col-6 pt-2 pb-2 rounded-left text-center border-div text-light" style={{height: '44px'}}>{this.state.price}</div>
+                                            <select className="col-6 pt-2 pb-2 rounded-right bg-warning text-center border border-warning text-light" name="type" onChange={this.handleChange2}>
+                                                <option type="0">XLM</option>
+                                                <option type="1">XIR</option>
+                                                <option type="2">IRR</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="col-sm-5 col-12 text-center">
                         <div className="row">
                             <h2 className="col-12 mb-2 font-weight-bold text-light">XIR</h2>
