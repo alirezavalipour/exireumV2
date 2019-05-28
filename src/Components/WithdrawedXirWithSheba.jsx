e88c9c9ba499852601807350e77927fb7c9c8c2c
mohammad <m.tehrani@gmail.com>
2019-05-28 11:55:09 +0430

    push

--- src/Components/WithdrawedXirWithSheba.jsx
+++ src/Components/WithdrawedXirWithSheba.jsx
@@ -389,14 +389,6 @@ class WithdrawedXirWithSheba extends Component {
                                     <InputMask maskChar="*" placeholder="IR-****-****-****-****-****-****" mask="IR-9999-9999-9999-9999-9999-9999" required='required' className="col-9 text-center rounded-right p-2 text-light" name="sheba" type="text" onChange={this.handleChange}/>
                                 </div>
                             </label>
-                            {/*<TextField*/}
-                                {/*outlined*/}
-                                {/*label='Name'*/}
-                            {/*>*/}
-                                {/*<Input*/}
-                                    {/*value={this.state.value}*/}
-                                    {/*onChange={(e) => this.setState({value: e.currentTarget.value})} />*/}
-                            {/*</TextField>*/}
                             <label className="col-12 mt-3">
                                 <div className="row shadow-lg">
                                     <span className="col-3 text-center text-light p-2 rounded-left bg-warning">Amount will be IRR (Rial)</span>
